34a22b92043be98b525c4ea1050df9daa0c2623d
bernardcryptohk <bernard.cryptohk@gmail.com>
2025-05-02 21:52:05 +0200

    feat: add event handling functions for button click and text input in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ type User = {
 }
 
 function App() {
+  
+  // typing event with react, just hover over the event and see what type it is
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) { // this type of event is going to change depending on the type of element you are using, in this case it is a button
+  }
+  function handleTextInput(e: React.ChangeEvent<HTMLInputElement>) {}
+
   // typing the useState hook
   const [count, setCount] = React.useState<number>(0) // 
   const [user, setUser] = React.useState<User | null>(null) // if it is async data from an api, it is possible to either get the user data or null, so we need to make the type as User or null
@@ -27,6 +33,11 @@ function App() {
   function alertMessage(message: string) {
     alert(message)
   }
+
+  const numbers = [1,2,3] as const // this defines the array as readonly, so you cannot change the values of the array
+  const obj = { name: 'John', age: 25 } as const // this defines the object as readonly, so you cannot change the values of the object
+  // This can be used with array and object to make sure that the values are readonly
+  //numbers[0] = 1
   
 
   return (
@@ -37,6 +48,9 @@ function App() {
         ))
       }
       {!loading && user && user.name}
+
+      <button onClick={(e)=>handleClick(e)}>Hello</button>
+      <input type="text" onChange={(e)=>handleTextInput(e)} />
     </>
   )
 }
